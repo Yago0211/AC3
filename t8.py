0e6a9e2489c28af49c4cd1045067ad4e608347da
Yago0211 <yago.santos@aluno.faculdadeimpacta.com.br>
2020-04-13 21:17:45 -0300

    Ac3

--- t8.py
+++ t8.py
@@ -5,11 +5,26 @@ from math import sqrt
 app = Flask(__name__)
 
 @app.route('/')
-def nao_entre_em_panico():
 
+def cemprimos():    
+    limite = 99
 
-    primos = "Tudo vai dar certo caros alunos!"
+    c = 1
+    p = 1
+    numero = 3
 
+    primos = "2,"
+
+    while p < limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            p += 1
+        numero+=1
 
     return primos
 
